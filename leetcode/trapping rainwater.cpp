e04894dd1fb8aef738e7e5ef56dce08932a22cca
ashutosh-kumar-dev <91617603+ashutosh-kumar-dev@users.noreply.github.com>
2024-07-01 22:24:58 +0530

    Update trapping rainwater.cpp
    added second method

--- leetcode/trapping rainwater.cpp
+++ leetcode/trapping rainwater.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+
+//Method 1
+
 int main(){
     int n=12;
     int arr[n]={0,1,0,2,1,0,1,3,2,1,2,1};
@@ -27,3 +30,36 @@ int main(){
         ans+=min(right[i],left[i])-arr[i];
 
 }
+
+
+//method 2 
+
+class Solution {
+public:
+    int trap(vector<int>& arr) {
+        
+        // using two pointer approach
+        int l =0;
+        int r = arr.size()-1;
+
+        int lmax=arr[0];
+        int rmax = arr[arr.size()-1];
+        int  waterSum = 0;
+        while(l<r){
+
+            if(arr[l] <= arr[r]){
+                lmax = max(lmax, arr[l]);
+                waterSum += lmax - arr[l];
+                l++;
+            }
+
+            else if(arr[l]>arr[r]){
+                rmax = max(rmax, arr[r]);
+                waterSum += rmax - arr[r];
+                r--;
+            }
+
+        }
+        return waterSum;
+    }
+};
